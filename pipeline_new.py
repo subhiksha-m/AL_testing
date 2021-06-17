df206bea6e6a90d699eab933fcf2acbd517d5d8a
subhiksha-m <47918614+subhiksha-m@users.noreply.github.com>
2021-06-17 22:59:09 +0800

    Update pipeline_new.py

--- pipeline_new.py
+++ pipeline_new.py
@@ -647,9 +647,9 @@ class Pipeline:
             #TODO add config path
             #tmp_df = pd.DataFrame(tmp_prob2, columns = [f'{iteration}'])
             self.prediction_prob[iteration]=tmp_prob2
-            print(self.prediction_prob)
+            print("prediciton_prob",self.prediction_prob)
             df = pd.DataFrame.from_dict(self.prediction_prob, orient='index').transpose()
-            df.to_csv("./prob.csv")
+            df.to_csv("/content/prob.csv")
 
 
             print(f"iteration {iteration} metrics = {self.metrics}")
