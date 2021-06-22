901a84143d73665d5eeacf1e78164a8e2069ca20
subhiksha-m <47918614+subhiksha-m@users.noreply.github.com>
2021-06-22 09:04:54 +0800

    Update ActiveLabeler.py

--- ActiveLabeler-main/ActiveLabeler.py
+++ ActiveLabeler-main/ActiveLabeler.py
@@ -34,7 +34,7 @@ class ActiveLabeler():
         if name == 'uncertainty':
             tmp_query = np.array([query[i][0] for i in range(len(query))])
             difference_array = np.absolute(tmp_query - 0.5 )
-            print("index", difference_array.argsort()[:N])
+            #print("index", difference_array.argsort()[:N])
             #uncertainty = [query[i][0] for i in index]
             return difference_array.argsort()[:N]
 
